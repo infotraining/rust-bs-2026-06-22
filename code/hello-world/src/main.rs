09aafe29fa1d95d2b794a7295ddae95d7158e9d4
Krystian Piękoś <krystian.piekos@infotraining.pl>
2026-06-22 13:58:18 +0000

    After 1st Day

--- code/hello-world/src/main.rs
+++ code/hello-world/src/main.rs
@@ -1,5 +1,38 @@
+static GREETING: &str = "Hello, world!";
+
+static mut COUNTER: u32 = 0;
+
+fn increment_counter() {
+    unsafe {
+        COUNTER += 1;
+    }
+}
+
+fn print_counter() {
+    unsafe {
+        println!("Counter: {}", COUNTER);
+    }
+}
+
 fn main() {
-    let mut message = "Hello, world!".to_string();
+    let mut message = GREETING.to_string();
     message += "!!";
     println!("{message}");
+
+    increment_counter();
+    print_counter();
+
+    let big_number: i32 = std::i32::MAX;
+
+    let flag = false;
+
+    let dx;
+    if flag {
+        dx = 1;
+    } else {
+        dx = 0;
+    }
+
+    let next_number = big_number + dx;
+    println!("Next number: {}", next_number);
 }
